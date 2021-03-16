a300ba4d1d68ae03c258d10c48c3a31677001cf2
IbrahimgilangR <80778648+IbrahimgilangR@users.noreply.github.com>
2021-03-17 00:09:11 +0700

    Add files via upload

---
+++ PBOtugas1gallon.java
@@ -0,0 +1,15 @@
+package pbotugas1gallon;
+import java.util.Scanner;
+
+public class PBOtugas1gallon {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int gallons;
+        System.out.print("input gallon : ");
+        gallons = input.nextInt();
+        double liters = gallons * 3.785;
+        System.out.println("\nNilai dari liters adalah "+liters);
+    }
+    
+}
